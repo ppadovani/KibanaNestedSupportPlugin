d40d0074d9ce25fe3834eda309c737a7c24c7a93
J$ <pierre@padovani.org>
2017-12-14 13:59:31 -0600

    Initial search parser intercept work #2

--- public/hack.js
+++ public/hack.js
@@ -1,38 +1,67 @@
-import _ from 'lodash';
-import $ from 'jquery';
+// import _ from 'lodash';
+// import $ from 'jquery';
 import * as parseQuery from 'ui/parse_query/parse_query';
 
-parseQuery.getDefaultQuery = function () {
+
+const oldGetDefaultQuery = parseQuery.getDefaultQuery;
+parseQuery.getDefaultQuery = function() {
   alert('boo!');
-  return { match_all: {} };
+  return oldGetDefaultQuery();
 };
 
-parseQuery.parseQuery = function (query) {
+const oldParseQuery = parseQuery.parseQuery;
+parseQuery.parseQuery = function(query) {
   alert('boo!');
-  if (!_.isString(query) || query.trim() === '') {
-    return parseQuery.getDefaultQuery();
-  }
-
-  try {
-    const parsedQuery = JSON.parse(query);
-    if (_.isObject(parsedQuery)) {
-      return parsedQuery;
-    }
-    return getTextQuery(query);
-  } catch (e) {
-    return getTextQuery(query);
-  }
-};
-
-function getTextQuery(query) {
-  return {
-    query_string: { query }
-  };
+  return oldParseQuery(query);
 }
 
+// var newParser = (function(object) {
+//
+//   object.getDefaultQuery = function($scope) {
+//     alert('boo!');
+//     return parseQuery.getDefaultQuery();
+//   };
+//
+//   object.parseQuery = function($scope, query) {
+//     alert('Boo!');
+//     return parseQuery.parseQuery(query);
+//   };
+//
+//   return object;
+//
+// }(newParser));
 
-$(document.body).on('keypress', function (event) {
-  if (event.which === 58) {
-    alert('boo!');
-  }
-});
+// parseQuery.getDefaultQuery = function () {
+//   alert('boo!');
+//   return { match_all: {} };
+// };
+//
+// parseQuery.parseQuery = function (query) {
+//   alert('boo!');
+//   if (!_.isString(query) || query.trim() === '') {
+//     return parseQuery.getDefaultQuery();
+//   }
+//
+//   try {
+//     const parsedQuery = JSON.parse(query);
+//     if (_.isObject(parsedQuery)) {
+//       return parsedQuery;
+//     }
+//     return getTextQuery(query);
+//   } catch (e) {
+//     return getTextQuery(query);
+//   }
+// };
+//
+// function getTextQuery(query) {
+//   return {
+//     query_string: { query }
+//   };
+// }
+//
+//
+// $(document.body).on('keypress', function (event) {
+//   if (event.which === 58) {
+//     alert('boo!');
+//   }
+// });
---
+++ public/hacks/parse_query/lib/from_user.js
@@ -0,0 +1,71 @@
+import _ from 'lodash';
+import jison from 'jison';
+import { DecorateQueryProvider } from 'ui/courier/data_source/_decorate_query';
+
+let bnf = require('raw!./query_lang.jison');
+let ngModel;
+let parser = new jison.Parser(bnf, {
+  type: 'slr',
+  noDefaultResolve: true,
+  moduleType: 'js'
+});
+parser.yy = require('./query_adapter');
+
+/**
+ * Take text from the user and make it into a query object
+ * @param {text} user's query input
+ * @returns {object}
+ */
+export function fromUser(text, model) {
+  function getQueryStringQuery(text) {
+    return DecorateQueryProvider({query_string: {query: text}});
+  }
+
+  let matchAll = getQueryStringQuery('*');
+  if (model !== undefined) {
+    ngModel = model;
+  }
+
+  ngModel.parseError = undefined;
+  // If we get an empty object, treat it as a *
+  if (_.isObject(text)) {
+    if (Object.keys(text).length) {
+      return text;
+    } else {
+      return matchAll;
+    }
+  }
+
+  // Nope, not an object.
+  text = (text || '').trim();
+  if (text.length === 0) return matchAll;
+
+  if (text[0] === '{') {
+    try {
+      return JSON.parse(text);
+    } catch (e) {
+      return getQueryStringQuery(text);
+    }
+  } else {
+    if (ngModel.$parent.indexPattern.nested) {
+      // try {
+        if (ngModel.filter) {
+          ngModel.filter.base_query = text;
+        }
+        let parsed = parser.parse(text).toJson();
+        return JSON.parse(parsed);
+      // } catch (e) {
+      //   ngModel.parseError = e.message;
+      //   return undefined;
+      // }
+    }
+    return getQueryStringQuery(text);
+  }
+}
+
+export function fromUserIndexPattern(fieldMap) {
+  parser.yy.fieldDictionary = fieldMap;
+}
+
+
+
