fe8947a4549c79b45dfa0646de70a28a3b7ca371
Pierre Padovani <pierre.padovani@civitaslearning.com>
2017-12-29 17:41:08 -0600

    Fix timelion support #16 (#17)

--- public/nested_support/discover/no_results.js
+++ public/nested_support/discover/no_results.js
@@ -4,10 +4,14 @@ import noResultsTemplate from '../../discover/partials/no_results.html';
 uiModules
   .get('apps/discover')
   .config(function ($provide) {
-    $provide.decorator('discoverNoResultsDirective', function ($delegate) {
+    try {
+      $provide.decorator('discoverNoResultsDirective', function ($delegate) {
 
-      let directive = $delegate[0];
-      directive.template = noResultsTemplate;
-      return $delegate;
-    });
+        let directive = $delegate[0];
+        directive.template = noResultsTemplate;
+        return $delegate;
+      });
+    } catch (e) {
+      // do nothing as the provider isn't there when someone selects timelion
+    }
   });
