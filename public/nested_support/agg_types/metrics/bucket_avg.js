93c1084342b0e39ee385ebb7033fca16c22c2f83
Pierre Padovani <pierre.padovani@civitaslearning.com>
2018-04-23 20:19:25 -0500

    Fixes #59 #64 #64 #65
    (cherry picked from commit 9a92dd3)
    
    Fix and release #65
    
    (cherry picked from commit 2f4eb66)
    
    (cherry picked from commit 7194143)
    
    (cherry picked from commit cdcc898)
    
    (cherry picked from commit 1e65cf7)
    
    (cherry picked from commit 5d86f13)

---
+++ public/nested_support/agg_types/metrics/bucket_avg.js
@@ -0,0 +1,29 @@
+import { ordinalSuffix } from 'ui/utils/ordinal_suffix';
+import percentsEditor from 'ui/agg_types/controls/percentiles.html';
+import 'ui/number_list';
+import { AggTypesMetricsMetricAggTypeProvider } from 'ui/agg_types/metrics/metric_agg_type';
+import { AggTypesMetricsGetResponseAggConfigClassProvider } from 'ui/agg_types/metrics/get_response_agg_config_class';
+import 'ui/courier';
+import { uiModules } from 'ui/modules';
+import { AggTypesMetricsBucketAvgProvider } from 'ui/agg_types/metrics/bucket_avg';
+
+let app = uiModules.get('kibana/courier');
+
+app.run(function(config, Private) {
+  const BucketAvgType = Private(AggTypesMetricsBucketAvgProvider);
+  BucketAvgType.getValue = function (agg, bucket) {
+	  let valueBucket = bucket;
+	  if (bucket['nested_' + agg.parentId]) {
+		  valueBucket = bucket['nested_' + agg.parentId];
+	  }
+      const customMetric = agg.params.customMetric;
+      const scaleMetrics = customMetric.type && customMetric.type.isScalable();
+
+      let value = valueBucket[agg.id] && valueBucket[agg.id].value;
+      if (scaleMetrics) {
+        const aggInfo = agg.params.customBucket.write();
+        value *= get(aggInfo, 'bucketInterval.scale', 1);
+      }
+      return value;
+  }
+});
