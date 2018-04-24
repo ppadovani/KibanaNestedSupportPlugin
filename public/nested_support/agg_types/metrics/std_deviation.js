4f7bbe8614a036eb3386ae54d32c9ea4c6fca88a
Pierre Padovani <pierre.padovani@civitaslearning.com>
2018-04-23 20:19:25 -0500

    Fixes #64 #64 #65
    (cherry picked from commit 9a92dd3)
    
    Fix and release #65
    
    (cherry picked from commit 2f4eb66)
    
    (cherry picked from commit 7194143)
    
    (cherry picked from commit cdcc898)

---
+++ public/nested_support/agg_types/metrics/std_deviation.js
@@ -0,0 +1,21 @@
+import { ordinalSuffix } from 'ui/utils/ordinal_suffix';
+import percentsEditor from 'ui/agg_types/controls/percentiles.html';
+import 'ui/number_list';
+import { AggTypesMetricsMetricAggTypeProvider } from 'ui/agg_types/metrics/metric_agg_type';
+import { AggTypesMetricsGetResponseAggConfigClassProvider } from 'ui/agg_types/metrics/get_response_agg_config_class';
+import 'ui/courier';
+import { uiModules } from 'ui/modules';
+import { AggTypesMetricsStdDeviationProvider } from 'ui/agg_types/metrics/std_deviation';
+
+let app = uiModules.get('kibana/courier');
+
+app.run(function(config, Private) {
+  const StdDeviationAggType = Private(AggTypesMetricsStdDeviationProvider);
+  StdDeviationAggType.getValue = function (agg, bucket) {
+	  let valueBucket = bucket;
+	  if (bucket['nested_' + agg.parentId]) {
+		  valueBucket = bucket['nested_' + agg.parentId];
+	  }
+      return _.get(valueBucket[agg.parentId], agg.valProp());
+  };
+});
