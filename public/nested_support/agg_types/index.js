5d86f13281b45d37e58168f2e9b454aed0ea3f64
Pierre Padovani <pierre.padovani@civitaslearning.com>
2018-04-23 20:19:25 -0500

    Fixes #59 #64 #64 #65
    (cherry picked from commit 9a92dd3)
    
    Fix and release #65
    
    (cherry picked from commit 2f4eb66)
    
    (cherry picked from commit 7194143)
    
    (cherry picked from commit cdcc898)
    
    (cherry picked from commit 1e65cf7)

--- public/nested_support/agg_types/index.js
+++ public/nested_support/agg_types/index.js
@@ -1,6 +1,11 @@
 import './agg_type';
 import './param_types/field';
 import './metrics/metric_agg_type';
+import './metrics/percentiles';
+import './metrics/bucket_avg';
+import './metrics/percentile_ranks';
+import './metrics/std_deviation';
+import './metrics/top_hit';
 import './buckets/terms';
 import './buckets/filters';
 import './buckets/create_filter/date_histogram';
---
+++ public/nested_support/agg_types/metrics/top_hit.js
@@ -0,0 +1,52 @@
+import { ordinalSuffix } from 'ui/utils/ordinal_suffix';
+import percentsEditor from 'ui/agg_types/controls/percentiles.html';
+import 'ui/number_list';
+import { AggTypesMetricsMetricAggTypeProvider } from 'ui/agg_types/metrics/metric_agg_type';
+import { AggTypesMetricsGetResponseAggConfigClassProvider } from 'ui/agg_types/metrics/get_response_agg_config_class';
+import 'ui/courier';
+import { uiModules } from 'ui/modules';
+import { AggTypesMetricsTopHitProvider } from 'ui/agg_types/metrics/top_hit';
+
+let app = uiModules.get('kibana/courier');
+
+app.run(function(config, Private) {
+  const TopHitType = Private(AggTypesMetricsTopHitProvider);
+  TopHitType.getValue = function (agg, bucket) {
+	  let valueBucket = bucket;
+	  if (bucket['nested_' + agg.parentId]) {
+		  valueBucket = bucket['nested_' + agg.parentId];
+	  }
+      const hits = _.get(valueBucket, `${agg.id}.hits.hits`);
+      if (!hits || !hits.length) {
+        return null;
+      }
+      const path = agg.params.field.name;
+
+      let values = _(hits).map(hit => {
+        return path === '_source' ? hit._source : agg.vis.indexPattern.flattenHit(hit, true)[path];
+      })
+        .flatten()
+        .value();
+
+      if (values.length === 1) {
+        values = values[0];
+      }
+
+      if (Array.isArray(values)) {
+        if (!_.compact(values).length) {
+          return null;
+        }
+        switch (agg.params.aggregate.val) {
+          case 'max':
+            return _.max(values);
+          case 'min':
+            return _.min(values);
+          case 'sum':
+            return _.sum(values);
+          case 'average':
+            return _.sum(values) / values.length;
+        }
+      }
+      return values;
+    }
+});
