2cb7fe852b047c5b696aab81aeefcc942b8b1413
Pierre Padovani <pierre.padovani@civitaslearning.com>
2018-05-05 15:07:30 -0500

    backport of #59 #63 #64 #65 along with improvements
    (cherry picked from commit 853ba49)
    
    Fix field format issue, update to new release
    
    (cherry picked from commit 6faea74)
    
    (cherry picked from commit dc06ba9)
    
    (cherry picked from commit 5b7bba4)
    
    (cherry picked from commit 97ad9f9)

---
+++ public/nested_support/agg_types/metrics/std_deviation.js
@@ -0,0 +1,18 @@
+import _ from 'lodash';
+import { AggTypesMetricsMetricAggTypeProvider } from 'ui/agg_types/metrics/metric_agg_type';
+import { AggTypesMetricsGetResponseAggConfigClassProvider } from 'ui/agg_types/metrics/get_response_agg_config_class';
+import { uiModules } from 'ui/modules';
+import { AggTypesMetricsStdDeviationProvider } from 'ui/agg_types/metrics/std_deviation';
+
+let app = uiModules.get('kibana/courier');
+
+app.run(function(config, Private) {
+  const StdDeviationAggType = Private(AggTypesMetricsStdDeviationProvider);
+  StdDeviationAggType.getValue = function (agg, bucket) {
+     let valueBucket = bucket;
+     if (bucket['nested_' + agg.parentId]) {
+         valueBucket = bucket['nested_' + agg.parentId];
+     }
+      return _.get(valueBucket[agg.parentId], agg.valProp());
+  };
+});
