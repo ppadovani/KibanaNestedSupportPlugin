56039a703a212d327bb44283bb0fa8aad177a018
Pierre Padovani <pierre.padovani@civitaslearning.com>
2018-08-08 08:16:13 -0500

    Fix #58... retry for up to five times to attempt to modify kibana index
    (cherry picked from commit 007ff15)
    
    (cherry picked from commit bbae238)
    
    (cherry picked from commit 65eb777)
    
    Fix #58, retry forever
    
    (cherry picked from commit eea73a4)
    
    (cherry picked from commit 198329a)
    
    (cherry picked from commit c38ef3f)
    
    (cherry picked from commit 3a8725d)
    
    (cherry picked from commit 67d557b)
    
    (cherry picked from commit 057056d)
    
    Update package.json

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 import {resolve} from 'path';
+import Promise from 'bluebird';
+
+function updateIndexSchema(callWithInternalUser, server) {
+  return callWithInternalUser('indices.putMapping', {
+    index: server.config().get('kibana.index'),
+    type: "doc",
+    body: {
+      properties: {
+        "index-pattern": {
+          properties: {
+            nested: {
+              type: "boolean"
+            }
+          }
+        }
+      }
+    }
+  }).catch(function (err) {
+    return Promise.delay(10).then(updateIndexSchema.bind(null, callWithInternalUser, server));
+  });
+}
+
 
 export default function (kibana) {
   return new kibana.Plugin({
     require: ['elasticsearch'],
     name: 'nested-fields-support',
     uiExports: {
-      
+
       docViews: ['plugins/nested-fields-support/nested_support/doc_view/structure'],
 
       managementSections: [
@@ -16,7 +38,8 @@ export default function (kibana) {
       hacks: [
         'plugins/nested-fields-support/nested_support'
       ]
-      
+
+
     },
 
     config(Joi) {
@@ -26,26 +49,12 @@ export default function (kibana) {
       }).default();
     },
 
-    
+
     // Update the .kibana index-pattern type to include a new nested flag
     init(server, options) {
-      const {callWithInternalUser} = server.plugins.elasticsearch.getCluster('admin');
+      const { callWithInternalUser } = server.plugins.elasticsearch.getCluster('admin');
 
-      callWithInternalUser('indices.putMapping', {
-        index: server.config().get('kibana.index'),
-        type: "doc",
-        body: {
-          properties: {
-            "index-pattern": {
-              properties: {
-                nested: {
-                  type: "boolean"
-    }
-              }
-            }
-          }
-        }
-      });
+      updateIndexSchema(callWithInternalUser, server);
 
       server.route({
         path: '/api/nested-fields-support/mappings/{name}',
